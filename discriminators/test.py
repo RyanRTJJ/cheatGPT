11232182eccebb23662914db50465139c9b5507a
Jacob Stavrianos <jstav@stanford.edu>
2023-03-01 17:11:32 -0800

    created json of reference passages

--- discriminators/test.py
+++ discriminators/test.py
@@ -4,6 +4,9 @@ creates and samples from a BERT-based discriminator
 uses predefined passages (human & machine generated)
 """
 
+# os for file pathing
+import os
+
 # imports discriminator class
 from BERT import BERT
 
@@ -13,4 +16,25 @@ discriminator = BERT()
 # defines passages
 # reads in each passage from refence_passages
 # stores each passage in a dict: name -> passage
+
+# WARNING: must be run from within project directory
+# run_path = os.path.dirname(os.path.realpath(__file__))
+run_path = os.getcwd()
+
+# truncates run_path to cheatGPT directory
+proj_path = run_path[:run_path.index("cheatGPT") + len("cheatGPT")]
+assert(proj_path.endswith("cheatGPT"))
+
+# defines path to reference_passages
+target_folder = "reference_passages"
+target_path = os.path.join(proj_path, target_folder)
+
+# reads in files from target_path
 passages = {}
+with os.scandir(target_path) as it:
+    for entry in it:
+        if not entry.name.startswith('.') and entry.is_file():
+            # reads in passage
+            with open(entry.path, 'r') as file:
+                passage = file.read()
+                passages[entry.name] = passage
