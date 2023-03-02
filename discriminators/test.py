80444bfe6fd3061a61adfc65947d4e63c971fcca
Jacob Stavrianos <jstav@stanford.edu>
2023-03-02 00:09:25 -0800

    goodnight

--- discriminators/test.py
+++ discriminators/test.py
@@ -7,34 +7,29 @@ uses predefined passages (human & machine generated)
 # os for file pathing
 import os
 
+# json for file I/O
+import json
+
 # imports discriminator class
 from BERT import BERT
+
+# imports json of passages as dict
+# WARNING: must be run from base project directory
+proj_path = os.getcwd()
+assert(proj_path.endswith("cheatGPT"))
+
+# defines path to reference_passages
+INPUT_FILE = "reference_passages.json"
+INPUT_PATH = os.path.join(proj_path, INPUT_FILE)
+passages = json.load(open(INPUT_PATH, 'r'))
 
 # initializes discriminator
 discriminator = BERT()
 
-# defines passages
-# reads in each passage from refence_passages
-# stores each passage in a dict: name -> passage
-
-# WARNING: must be run from within project directory
-# run_path = os.path.dirname(os.path.realpath(__file__))
-run_path = os.getcwd()
-
-# truncates run_path to cheatGPT directory
-proj_path = run_path[:run_path.index("cheatGPT") + len("cheatGPT")]
-assert(proj_path.endswith("cheatGPT"))
-
-# defines path to reference_passages
-target_folder = "reference_passages"
-target_path = os.path.join(proj_path, target_folder)
-
-# reads in files from target_path
-passages = {}
-with os.scandir(target_path) as it:
-    for entry in it:
-        if not entry.name.startswith('.') and entry.is_file():
-            # reads in passage
-            with open(entry.path, 'r') as file:
-                passage = file.read()
-                passages[entry.name] = passage
+# BROKEN
+# tests discriminator on reference passages
+for filename, passage in passages.items():
+    print("\n\t FILENAME: " + filename)
+    print("\n\t PASSAGE: " + passage)
+    classification = discriminator.discriminate(passage)
+    print("\n\t CLASSIFICATION: " + classification)
