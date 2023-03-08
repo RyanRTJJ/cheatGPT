130baefe5d578593863ca7db5ca352d3cebc32ad
raghav <rmgarg@stanford.edu>
2023-03-08 03:36:33 +0000

    set up data pipeline

---
+++ discriminators/uniform-pt/run.py
@@ -0,0 +1,267 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import datasets
+import transformers
+import re
+import torch
+import torch.nn.functional as F
+import tqdm
+import random
+from sklearn.metrics import roc_curve, precision_recall_curve, auc
+import argparse
+import datetime
+import os
+import json
+import functools
+import custom_datasets
+from multiprocessing.pool import ThreadPool
+import time
+
+def load_base_model():
+    print('MOVING BASE MODEL TO GPU...', end='', flush=True)
+    start = time.time()
+    try:
+        mask_model.cpu()
+    except NameError:
+        pass
+    if args.openai_model is None:
+        base_model.to(DEVICE)
+    print(f'DONE ({time.time() - start:.2f}s)')
+
+def load_base_model_and_tokenizer(name):
+    if args.openai_model is None:
+        print(f'Loading BASE model {args.base_model_name}...')
+        base_model_kwargs = {}
+        if 'gpt-j' in name or 'neox' in name:
+            base_model_kwargs.update(dict(torch_dtype=torch.float16))
+        if 'gpt-j' in name:
+            base_model_kwargs.update(dict(revision='float16'))
+        base_model = transformers.AutoModelForCausalLM.from_pretrained(name, **base_model_kwargs, cache_dir=cache_dir)
+    else:
+        base_model = None
+
+    optional_tok_kwargs = {}
+    if "facebook/opt-" in name:
+        print("Using non-fast tokenizer for OPT")
+        optional_tok_kwargs['fast'] = False
+    if args.dataset in ['pubmed']:
+        optional_tok_kwargs['padding_side'] = 'left'
+    base_tokenizer = transformers.AutoTokenizer.from_pretrained(name, **optional_tok_kwargs, cache_dir=cache_dir)
+    base_tokenizer.pad_token_id = base_tokenizer.eos_token_id
+
+    return base_model, base_tokenizer
+
+# strip newlines from each example; replace one or more newlines with a single space
+def strip_newlines(text):
+    return ' '.join(text.split())
+
+def generate_data(dataset, key):
+    # load data
+    if dataset in custom_datasets.DATASETS:
+        data = custom_datasets.load(dataset, cache_dir)
+    else:
+        data = datasets.load_dataset(dataset, split='train', cache_dir=cache_dir)[key]
+    
+    # get unique examples, strip whitespace, and remove newlines
+    # then take just the long examples, shuffle, take the first 5,000 to tokenize to save time
+    # then take just the examples that are <= 512 tokens (for the mask model)
+    # then generate n_samples samples
+
+    # remove duplicates from the data
+    data = list(dict.fromkeys(data))  # deterministic, as opposed to set()
+
+    # strip whitespace around each example
+    data = [x.strip() for x in data]
+
+    # remove newlines from each example
+    data = [strip_newlines(x) for x in data]
+
+    # try to keep only examples with > 250 words
+    if dataset in ['writing', 'squad', 'xsum']:
+        long_data = [x for x in data if len(x.split()) > 250]
+        if len(long_data) > 0:
+            data = long_data
+
+    random.seed(0)
+    random.shuffle(data)
+
+    data = data[:5_000]
+
+    # keep only examples with <= 512 tokens according to mask_tokenizer
+    # this step has the extra effect of removing examples with low-quality/garbage content
+    tokenized_data = preproc_tokenizer(data)
+    data = [x for x, y in zip(data, tokenized_data["input_ids"]) if len(y) <= 512]
+
+    # print stats about remainining data
+    print(f"Total number of samples: {len(data)}")
+    print(f"Average number of words: {np.mean([len(x.split()) for x in data])}")
+
+    return generate_samples(data[:n_samples], batch_size=batch_size)
+
+if __name__ == '__main__':
+    DEVICE = "cuda"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, default="xsum")
+    parser.add_argument('--dataset_key', type=str, default="document")
+    parser.add_argument('--n_samples', type=int, default=500)
+    parser.add_argument('--n_perturbation_list', type=str, default="50")
+    parser.add_argument('--base_model_name', type=str, default="gpt2-medium")
+    parser.add_argument('--mask_filling_model_name', type=str, default="t5-large")
+    parser.add_argument('--batch_size', type=int, default=20)
+    parser.add_argument('--do_top_k', action='store_true')
+    parser.add_argument('--top_k', type=int, default=40)
+    parser.add_argument('--do_top_p', action='store_true')
+    parser.add_argument('--top_p', type=float, default=0.96)
+    parser.add_argument('--output_name', type=str, default="")
+    parser.add_argument('--openai_model', type=str, default=None)
+    parser.add_argument('--openai_key', type=str)
+    parser.add_argument('--mask_top_p', type=float, default=1.0)
+    parser.add_argument('--cache_dir', type=str, default="~/.cache")
+    args = parser.parse_args()
+
+    API_TOKEN_COUNTER = 0
+
+    if args.openai_model is not None:
+        import openai
+        assert args.openai_key is not None, "Must provide OpenAI API key as --openai_key"
+        openai.api_key = args.openai_key
+
+    START_DATE = datetime.datetime.now().strftime('%Y-%m-%d')
+    START_TIME = datetime.datetime.now().strftime('%H-%M-%S-%f')
+
+    # define SAVE_FOLDER as the timestamp - base model name - mask filling model name
+    # create it if it doesn't exist
+    sampling_string = "top_k" if args.do_top_k else ("top_p" if args.do_top_p else "temp")
+    output_subfolder = f"{args.output_name}/" if args.output_name else ""
+    if args.openai_model is None:
+        base_model_name = args.base_model_name.replace('/', '_')
+    else:
+        base_model_name = "openai-" + args.openai_model.replace('/', '_')
+    SAVE_FOLDER = f"tmp_results/{output_subfolder}{base_model_name}-{args.mask_filling_model_name}-{sampling_string}/{START_DATE}-{START_TIME}-{args.dataset}-{args.n_samples}"
+    if not os.path.exists(SAVE_FOLDER):
+        os.makedirs(SAVE_FOLDER)
+    print(f"Saving results to absolute path: {os.path.abspath(SAVE_FOLDER)}")
+
+    # write args to file
+    with open(os.path.join(SAVE_FOLDER, "args.json"), "w") as f:
+        json.dump(args.__dict__, f, indent=4)
+
+    mask_filling_model_name = args.mask_filling_model_name
+    n_samples = args.n_samples
+    batch_size = args.batch_size
+    n_perturbation_list = [int(x) for x in args.n_perturbation_list.split(",")]
+
+    cache_dir = args.cache_dir
+    os.environ["XDG_CACHE_HOME"] = cache_dir
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+    print(f"Using cache dir {cache_dir}")
+
+    GPT2_TOKENIZER = transformers.GPT2Tokenizer.from_pretrained('gpt2', cache_dir=cache_dir)
+
+    # generic generative model
+    base_model, base_tokenizer = load_base_model_and_tokenizer(args.base_model_name)
+
+    # mask filling t5 model
+    print(f'Loading mask filling model {mask_filling_model_name}...')
+    mask_model = transformers.AutoModelForSeq2SeqLM.from_pretrained(mask_filling_model_name, cache_dir=cache_dir)
+    try:
+        n_positions = mask_model.config.n_positions
+    except AttributeError:
+        n_positions = 512
+
+    preproc_tokenizer = transformers.AutoTokenizer.from_pretrained('t5-small', model_max_length=512, cache_dir=cache_dir)
+    mask_tokenizer = transformers.AutoTokenizer.from_pretrained(mask_filling_model_name, model_max_length=n_positions, cache_dir=cache_dir)
+    if args.dataset in ['english', 'german']:
+        preproc_tokenizer = mask_tokenizer
+
+    load_base_model()
+
+    print(f'Loading dataset {args.dataset}...')
+    data = generate_data(args.dataset, args.dataset_key)
+    if args.random_fills:
+        FILL_DICTIONARY = set()
+        for texts in data.values():
+            for text in texts:
+                FILL_DICTIONARY.update(text.split())
+        FILL_DICTIONARY = sorted(list(FILL_DICTIONARY))
+
+    if args.scoring_model_name:
+        print(f'Loading SCORING model {args.scoring_model_name}...')
+        del base_model
+        del base_tokenizer
+        torch.cuda.empty_cache()
+        base_model, base_tokenizer = load_base_model_and_tokenizer(args.scoring_model_name)
+        load_base_model()  # Load again because we've deleted/replaced the old model
+
+    # write the data to a json file in the save folder
+    with open(os.path.join(SAVE_FOLDER, "raw_data.json"), "w") as f:
+        print(f"Writing raw data to {os.path.join(SAVE_FOLDER, 'raw_data.json')}")
+        json.dump(data, f)
+
+    if not args.skip_baselines:
+        baseline_outputs = [run_baseline_threshold_experiment(get_ll, "likelihood", n_samples=n_samples)]
+        if args.openai_model is None:
+            rank_criterion = lambda text: -get_rank(text, log=False)
+            baseline_outputs.append(run_baseline_threshold_experiment(rank_criterion, "rank", n_samples=n_samples))
+            logrank_criterion = lambda text: -get_rank(text, log=True)
+            baseline_outputs.append(run_baseline_threshold_experiment(logrank_criterion, "log_rank", n_samples=n_samples))
+            entropy_criterion = lambda text: get_entropy(text)
+            baseline_outputs.append(run_baseline_threshold_experiment(entropy_criterion, "entropy", n_samples=n_samples))
+
+        baseline_outputs.append(eval_supervised(data, model='roberta-base-openai-detector'))
+        baseline_outputs.append(eval_supervised(data, model='roberta-large-openai-detector'))
+
+    outputs = []
+
+    if not args.baselines_only:
+        # run perturbation experiments
+        for n_perturbations in n_perturbation_list:
+            perturbation_results = get_perturbation_results(args.span_length, n_perturbations, n_samples)
+            for perturbation_mode in ['d', 'z']:
+                output = run_perturbation_experiment(
+                    perturbation_results, perturbation_mode, span_length=args.span_length, n_perturbations=n_perturbations, n_samples=n_samples)
+                outputs.append(output)
+                with open(os.path.join(SAVE_FOLDER, f"perturbation_{n_perturbations}_{perturbation_mode}_results.json"), "w") as f:
+                    json.dump(output, f)
+
+    if not args.skip_baselines:
+        # write likelihood threshold results to a file
+        with open(os.path.join(SAVE_FOLDER, f"likelihood_threshold_results.json"), "w") as f:
+            json.dump(baseline_outputs[0], f)
+
+        if args.openai_model is None:
+            # write rank threshold results to a file
+            with open(os.path.join(SAVE_FOLDER, f"rank_threshold_results.json"), "w") as f:
+                json.dump(baseline_outputs[1], f)
+
+            # write log rank threshold results to a file
+            with open(os.path.join(SAVE_FOLDER, f"logrank_threshold_results.json"), "w") as f:
+                json.dump(baseline_outputs[2], f)
+
+            # write entropy threshold results to a file
+            with open(os.path.join(SAVE_FOLDER, f"entropy_threshold_results.json"), "w") as f:
+                json.dump(baseline_outputs[3], f)
+        
+        # write supervised results to a file
+        with open(os.path.join(SAVE_FOLDER, f"roberta-base-openai-detector_results.json"), "w") as f:
+            json.dump(baseline_outputs[-2], f)
+        
+        # write supervised results to a file
+        with open(os.path.join(SAVE_FOLDER, f"roberta-large-openai-detector_results.json"), "w") as f:
+            json.dump(baseline_outputs[-1], f)
+
+        outputs += baseline_outputs
+
+    save_roc_curves(outputs)
+    save_ll_histograms(outputs)
+    save_llr_histograms(outputs)
+
+    # move results folder from tmp_results/ to results/, making sure necessary directories exist
+    new_folder = SAVE_FOLDER.replace("tmp_results", "results")
+    if not os.path.exists(os.path.dirname(new_folder)):
+        os.makedirs(os.path.dirname(new_folder))
+    os.rename(SAVE_FOLDER, new_folder)
+
+    print(f"Used an *estimated* {API_TOKEN_COUNTER} API tokens (may be inaccurate)")
